46d307a7a8d57823cef534b093a66309f95400ef
Josiah Johnson <jjohnson@paylynxs.com>
2026-01-10 12:57:51 -0800

    try a simpler way to install chrome

--- files/scripts/enable-chrome-yum-repo.sh
+++ files/scripts/enable-chrome-yum-repo.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
 set -oue pipefail 
-# Part of an attempt to add Google Chrome in the usual way.
-echo "Fixing google-chrome yum repo"
-sed -i '/enabled/d' /etc/yum.repos.d/google-chrome.repo 
-echo "enabled=1" >> /etc/yum.repos.d/google-chrome.repo
 
-# This does not appear to be necessary, since at this point there are no
-# Google keys in the RPM database.  Will be deleted soon.
+### Install Google Chrome from Official Repository
+echo "Installing Google Chrome..."
 
-# First, delete all old keys; see https://github.com/rpm-software-management/rpm/issues/2577
-# echo "Fixing issues with Google GPG keys"
-# set +oue
-# rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs
-# GOOGLE_PUBKEYS_RPMS=$(rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs)
-# set -oue
-# echo "Installed pubkeys RPMS are $GOOGLE_PUBKEYS_RPMS"
-# if [ -n "$GOOGLE_PUBKEYS_RPMS" ]; then
-#     echo "Removing pakcages $GOOGLE_PUBKEYS_RPMS"
-#     rpm -e $GOOGLE_PUBKEYS_RPMS
-# fi
+# Add Google Chrome RPM repository
+cat > /etc/yum.repos.d/google-chrome.repo << 'EOF'
+[google-chrome]
+name=google-chrome
+baseurl=https://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
 
-# We need to download and install the Google signing keys separately, we can't trust
-# rpm-ostree to do it cleanly from the yum repo directly.
-# Possibly related to https://github.com/rpm-software-management/rpm/issues/2577
+# Install Chrome
+dnf5 install -y google-chrome-stable
 
-echo "Downloading Google Signing Key"
-curl https://dl.google.com/linux/linux_signing_key.pub > /tmp/linux_signing_key.pub
+# Clean up repo file (required - repos don't work at runtime in bootc images)
+rm -f /etc/yum.repos.d/google-chrome.repo
 
-rpm --import /tmp/linux_signing_key.pub
+echo "Google Chrome installed successfully"
