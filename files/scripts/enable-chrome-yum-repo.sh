b239f77c59c4d01ea7eab1d0122ce66d45b130db
Josiah Johnson <jjohnson@paylynxs.com>
2026-01-10 11:54:51 -0800

    fix commit script

--- files/scripts/enable-chrome-yum-repo.sh
+++ files/scripts/enable-chrome-yum-repo.sh
@@ -2,9 +2,9 @@
 
 # Part of an attempt to add Google Chrome in the usual way.
 echo "Fixing google-chrome yum repo"
-sed -i '/enabled/d' /etc/yum.repos.d/google-chrome.repo 
+sed -i '/^enabled=/d' /etc/yum.repos.d/google-chrome.repo
 echo "enabled=1" >> /etc/yum.repos.d/google-chrome.repo
-First, delete all old keys; see https://github.com/rpm-software-management/rpm/issues/2577
+# First, delete all old keys; see https://github.com/rpm-software-management/rpm/issues/2577
 GOOGLE_PUBKEYS_RPMS=$(rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs rpm -e)
 if [ -n $GOOGLE_PUBKEYS_RPMS ]; then
     echo "Removing pakcages $GOOGLE_PUBKEYS_RPMS"
